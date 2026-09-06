15affc88ecf3641ee63b55c273eece3acbcb4461
Gap0507 <shahgarv0507@gmail.com>
2026-09-06 19:58:42 +0530

    build issues -1

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 import path from "path";
 
 const nextConfig: NextConfig = {
-  outputFileTracingRoot: path.resolve(__dirname),
   reactCompiler: true,
   allowedDevOrigins: ["192.168.31.244"],
   serverExternalPackages: ["mongoose"],
